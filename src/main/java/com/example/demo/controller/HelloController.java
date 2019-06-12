c665e4f7b47f3e9d43e9b2faad0b660348376d2d
hansen <1304534326@qq.com>
2019-06-12 17:50:10 +0800

    配置文件自定义修改；

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.People;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableAutoConfiguration
 public class HelloController {
 
+    @Value("${common.name}")
+    private String name;
+
+    @Value("${common.age}")
+    private String age;
+
+    @Value("${common.content}")
+    private String content;
+
+    @Autowired
+    private People people;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "hello, springboot 11";
+        return "hello, springboot 11" + name + "," + age + "," + content + ",people.getAge=" + people.getAge();
     }
 }
